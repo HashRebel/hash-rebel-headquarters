0484377e9c495978ef64ff56a6f9ceaa4dbb38b9
Brian Henze <brian@goldenvolunteer.com>
2022-03-01 13:15:16 -0600

    feat: adding contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Brian Scott Henze
+      </h1>
+      {/* Add HTML for links here */}
+      <ul>
+        <li>Phone: (480) 747-0781</li>
+        <li>Email: brian@hashrebel.com</li>
+        <li>Mastodon: </li>
+        <li>Minds?</li>
+        <li>LYBR/Odysee: </li>
+        <li>Signal?Telegram?</li>
+      </ul>
+    </div>
   )
 }
